6f9c2fe72eec85fcdfeea183507d7b6df1fa7e42
HP <HP@DESKTOP-NMBILJD>
2024-11-02 20:09:23 +0100

    Add project files.

---
+++ engry code );/engry code );.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	
+	
+	int num1, num2;
+	int temp = num1;
+	num1 = num2;
+	num2 = temp;
+
+	cout << "num1" << endl;
+	cin >> num1;
+	cout << "num2" << endl;
+	cin >> num2;
+
+	
+	
+
+
+	return 0;
+
+
+}
